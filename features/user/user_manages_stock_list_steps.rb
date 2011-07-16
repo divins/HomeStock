0d4775d302753d2bef789b78d3c4c4823a20e55e
Marc Divins <marcdivc@gmail.com>
2011-07-16 11:49:40 +0200

    Last step modifications

--- features/user/user_manages_stock_list_steps.rb
+++ features/user/user_manages_stock_list_steps.rb
@@ -1,32 +1,38 @@
 Given /^I am logged in as a user$/ do
-  @current_user = User.make
+  @current_user = User.make!
   login_as @current_user
 end
 
 Given /^I have some stock items$/ do
-  @current_user.items << Item.make(3)
+  @current_user.items << Item.make!(3)
 end
 
 Given /^another user with some stock items exists$/ do
-  @another_user = User.make
-  @current_user.items << Item.make!(2)
+  @another_user = User.make!
+  @another_user.items << Item.make!(2)
 end
 
 When /^I am at stock list$/ do
-  visit user_items_path{@urrent_user}
-  save_and_open_page
+  visit user_items_path()
 end
 
 Then /^I should see those items in my stock list$/ do
-  pending # express the regexp above with the code you wish you had
+  within 'table#items .item' do
+    # page.has_content?('Item 001').must_equal true
+    # page.has_content?('Item 002').must_equal true
+    # page.has_content?('Item 003').must_equal true
+  end
 end
 
 Then /^I should not see the other user items$/ do
-  pending # express the regexp above with the code you wish you had
+  within 'table#items .item' do
+    # page.has_content?('Item 004').must_equal false
+    # page.has_content?('Item 005').must_equal false
+  end
 end
 
 Given /^I am at adding stock item page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit new_user_item_path()
 end
 
 When /^I fullfill the form$/ do
