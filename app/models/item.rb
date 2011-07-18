021fe2af8992a413298656e7b7089cb13a7ba991
divins <marcdivc@gmail.com>
2011-07-18 22:43:41 +0200

    Added validations and test to Items. WIP

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,22 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
+  validates :short_description, presence: true
+  validates :desired_stock, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :actual_stock, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :category, presence: true
+  validate :check_category
+
   def plus_one!
     update_attribute(:actual_stock, (actual_stock+1))
   end
   def minus_one!
     update_attribute(:actual_stock, (actual_stock-1))
+  end
+
+  private
+
+  def check_category
+    errors.add :category, :invalid_category unless Categories.all.include? self.category
   end
 end
--- test/unit/models/item_test.rb
+++ test/unit/models/item_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
-class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Item do
+  before do
+    @item = Item.new
+  end
+
+  describe "associations" do
+    it "belongs to an user" do
+      @item.must_respond_to :user
+    end
+  end
+
+  describe "validations" do
+    it "is not valid without a category" do
+      @item.category = nil
+      @item.valid?.must_equal false
+    end
+    it "is not valid with an invalid category" do
+      @item.category = "meat"
+      @item.valid?.must_equal true
+    end
+    it "is not valid without a short description" do
+      @item.short_description = nil
+      @item.valid?.must_equal false
+    end
+    it "is not valid without desired stock" do
+      @item.desired_stock = nil
+      @item.valid?.must_equal false
+    end
+    it "is not valid with subzero desired stock" do
+      @item.desired_stock = -1
+      @item.valid?.must_equal false
+    end
+    it "is not valid without actual stock" do
+      @item.actual_stock = nil
+      @item.valid?.must_equal false
+    end
+    it "is not valid with subzero actual stock" do
+      @item.actual_stock = -1
+      @item.valid?.must_equal true
+    end
+  end
 end
