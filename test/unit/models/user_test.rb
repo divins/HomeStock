dd56a04b6856209c2e94f0684d39e0743e64d117
Marc Divins <marcdivc@gmail.com>
2011-07-22 16:14:30 +0200

    Fixed user and added test. Closes #9

--- test/unit/models/user_test.rb
+++ test/unit/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe User do
+  before do
+    @user = User.make
+  end
+
+  describe 'relations' do
+    it 'has_many items' do
+      @user.must_respond_to :items
+    end
+  end
+
+  describe 'validations' do
+    it 'is not valid withoud an email' do
+      @user.email = nil
+      @user.valid?.must_equal false
+    end
+    it 'is not valid if email already exists' do
+      @user.save!
+      @repeated_user = User.make(email: @user.email)
+      @repeated_user.valid?.must_equal false
+    end
+  end
 end
